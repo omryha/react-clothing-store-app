e21540628208e7b05301a2c78babeea94a55ae25
hezy laplacian <hezylaplacian@gmail.com>
2021-01-01 13:20:01 +0200

    Initial commit

---
+++ client/src/pages/shop/shop.component.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+
+import CollectionPageContainer from '../collection/collection.container';
+import collectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
+
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
+
+  return (
+    <div className='shop-page'>
+      <Route
+        exact
+        path={`${match.path}`}
+        component={collectionsOverviewComponent}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
